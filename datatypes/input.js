0a286fbba8a79bab545baf3fab32d24e64e2e628
kumail <kumail112113@gmail.com>
2025-12-26 23:32:19 +0500

    Im tired today

---
+++ datatypes/input.js
@@ -0,0 +1,4 @@
+function submitForm(name, email, password) {
+    console.log("button was clicked");
+    console.log('name' + name + "email" + email + "password " + password);
+}
---
+++ datatypes/input.ts
@@ -0,0 +1,4 @@
+function submitForm(name: string, email: string, password: string): void {
+    console.log("button was clicked")
+    console.log('name' + name + "email" + email + "password " + password)
+}
