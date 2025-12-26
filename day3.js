08fc1acc57f2303f374951d0769f36acc1705eb1
kumail <kumail112113@gmail.com>
2025-12-26 23:04:41 +0500

    done for today

---
+++ day3.js
@@ -0,0 +1,4 @@
+var num = 10;
+var str = "Hello, TypeScript!";
+var sumi = num + 20;
+console.log("Sum:", sumi);
---
+++ day3.ts
@@ -0,0 +1,5 @@
+var num: number = 10;
+var str: string = "Hello, TypeScript!";
+
+var sumi: number = num + 20;
+console.log("Sum:", sumi);
