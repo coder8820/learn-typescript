fd2986a24036d7852e3b2ada3a2da1fce42856c1
kumail <kumail112113@gmail.com>
2025-12-26 19:23:15 +0500

    there are many types i understand like enum, alia,union,intersectin,leteral

--- day2.ts
+++ day2.ts
@@ -13,3 +13,43 @@ var bigInt: bigint = 123456789012345678901234567890n;
 
 // 2- Object Data Types
 //   a. Arrays, Functions, Classes, Interfaces, Objects
+
+let arr: number[] = [22, 33, 44, 55];
+// tuple
+let tuple: [string, number] = ["Age", 30];
+// Object
+let obj: {
+    name: string;
+    age: number;
+} = {
+    name: "Alice",
+    age: 25,
+}
+
+// 3- Special Data Types
+//   a. Any, Unknown, Never, Void
+
+let anyVar: any = "Could be anything";
+let unknown: unknown = 10;
+
+// 4- Advanced types
+//   a. Union, Intersection, Literal, Type Aliases, Enums
+// a variable that can hold either a string or a number
+let unionVar: string | number;
+unionVar = "A string";
+unionVar = 100;
+// Type Alias
+let aleasUser: User;
+type User = {
+    id: number;
+    username: string;
+}
+// Enum
+enum Direction{
+    Up,
+    Down,
+    Left,
+    Right
+}
+let dir: Direction = Direction.Up;
+console.log(dir);
