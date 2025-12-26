f8ad375fbe59d1834cd986de3cabadc07a60139c
kumail <kumail112113@gmail.com>
2025-12-26 19:12:05 +0500

    Understainding data types in typescript

---
+++ day2.ts
@@ -0,0 +1,15 @@
+// Data types in TypeScript
+// There are 4 main categories of data types in TypeScript:
+
+// 1- Primitive Data Types
+//   a. Number, String, Boolean, Null, Undefined, Symbol, BigInt
+var num: number = 42;
+var str: string = "Hello, TypeScript!";
+var bool: boolean = true;
+var nullVal: null = null;
+var undefinedVal: undefined = undefined;
+var sym: symbol = Symbol("key");
+var bigInt: bigint = 123456789012345678901234567890n;
+
+// 2- Object Data Types
+//   a. Arrays, Functions, Classes, Interfaces, Objects
